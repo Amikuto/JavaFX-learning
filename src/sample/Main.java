109b2bd7d6d531ce20d12901d9d156c2f9346bb5
Amikuto <Amikuto@users.noreply.github.com>
2021-02-18 12:57:21 +0300

    second commit: added Person, PersonOverviewControllerver, connected Main and PersonOverviewController.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,17 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
+import sample.controllers.PersonOverviewController;
+import sample.modules.Person;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -15,15 +19,12 @@ public class Main extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
-
+    private final ObservableList<Person> personData = FXCollections.observableArrayList();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Address");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
 
         initRootLayout();
 
@@ -32,9 +33,12 @@ public class Main extends Application {
 
     public void initRootLayout(){
         try{
+            //Корневой макет
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("views/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
+
+            //Сцена
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -44,16 +48,42 @@ public class Main extends Application {
     }
 
     public void showPersonOverview(){
-        try{
+        try {
+            // Загрузка сведений людей
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("views/main.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Помещаем сведения в центр корневого макета
             rootLayout.setCenter(personOverview);
+
+            // Даем доступ контроллеру к главному приложению
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public Main(){
+//        for (int i = 0; i<25; i++){
+//            personData.add(new Person("Name" + i, "Surename " + i));
+//        }
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/sample/controllers/PersonOverviewController.java
@@ -0,0 +1,53 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import sample.Main;
+import sample.modules.Person;
+
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTableView;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    private Main mainApp;
+
+    public PersonOverviewController(){}
+
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+
+        //Добавление данных
+        personTableView.setItems(mainApp.getPersonData());
+    }
+
+    @FXML
+    private void initialize(){
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().getFirstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().getLastNameProperty());
+
+//        showPersonData(null);
+
+//        personTableView.getSelectedModel()
+    }
+
+//    public void setMainapp
+}
